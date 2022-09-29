ec896eb0d330b3ce20b232a012523de295aee8b9
Gariest Gary <rtutede@gmail.com>
2022-09-30 02:14:45 +0500

    Initial commit

---
+++ Assets/Interface Package/Interface/ICharacterView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICharacterView
+{
+    public void PlayJump();
+    public void PlayLanding();
+    public void PlayIdle();
+}
